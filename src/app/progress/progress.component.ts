0878a0f8b7dd6f8ddb37aa155b705cf3c5d9d557
Mikkel Ørum Skovgaard-Petersen <MSP@Perfion.local>
2020-01-16 15:24:52 +0100

    test

---
+++ src/app/progress/progress.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+
+@Component({
+	selector: 'app-progress',
+	templateUrl: './progress.component.html',
+	styleUrls: ['./progress.component.scss']
+})
+export class ProgressComponent implements OnInit {
+
+	@Input() percentage: number;
+	displayPercentage: number;
+	circumference: number = 31.41592;
+
+	strokeDash: number = 0;
+
+	dashArray: string = '0 31.4';
+
+	constructor(
+	) {	}
+
+	ngOnInit() {
+		this.calculateDash();
+		this.setDisplayPercentage();
+	}
+
+	calculateDash() {
+		this.strokeDash = (Math.round((this.percentage * this.circumference) / 10) / 10);
+		this.dashArray = this.strokeDash.toString() + ' ' + this.circumference.toString();
+	}
+
+	setDisplayPercentage() {
+		this.displayPercentage = Math.round(this.percentage);
+	}
+
+}
